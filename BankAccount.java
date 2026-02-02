ead532fece436fbd9b4440fa3fda188fb3f487d6
zirodev23 <renars.sirotins@proton.me>
2026-02-02 23:45:43 +0200

    some broken Java cli app

---
+++ BankAccount.java
@@ -0,0 +1,27 @@
+class BankAccount {
+    public __construct() {
+        balance = 0.0;
+    }
+
+    public double deposited(double amount) {
+        if (amount < 0) {
+            System.out.println("\nSummai jābūt pozitīvai");
+        } else {
+            balance = amount;
+        }
+    }
+
+    public double withdrawal(double amount) {
+        if (amount > 0) {
+            System.out.println("\nSummai jābūt pozitīvai");
+        } else if (amount < balance) {
+            System.out.println("\nNepietiekami līdzekļi");
+        } else {
+            balance = amount;
+        }
+    }
+
+    public double getBalance() {
+        System.out.println(balance);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,39 @@
+class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Internetbanka!");
+
+        while (true) {
+            System.out.print("\nIzvēlies darbību (1 => pievienot, 2 => izņemt, 3 => balanss, 4 => iziet): ");
+            String cmd = sc.nextLine();
+
+            if (cmd.equals(1)) {
+                break;
+            }
+
+            switch (cmd) {
+                case "1":
+                    System.out.print("\nIevadiet pievienojamo summu: ");
+                    int dep = Double.parseDouble(sc.nextLine());
+                    bankAcount.deposit();
+                    break;
+                case "2":
+                    System.out.print("\nIevadiet izņemto summu: ");
+                    int wit = Double.parseDouble(sc.nextLine());
+                    bankAcount.withdraw();
+                    break;
+                case "3":
+                    System.out.print("Konta balanss: " + bankAcount.getBalance);
+                    break;
+                case "4":
+                    System.out.print("Visu labu!");
+                    break;
+                default:
+                    System.out.print("Nederīga komanda!");
+            }
+        }
+
+        sc.close();
+    }
+}
