e2c90bd526518e53e46bbcb327649d4138ee2557
Līga <asikeligalillija@gmail.com>
2026-02-03 15:24:49 +0200

    Fixed error: cannot find symbol in BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,27 +1,27 @@
 class BankAccount {
-    public __construct() {
-        balance = 0.0;
+     public static void __construct() {
+        double balance = 0.0;
     }
 
-    public double deposited(double amount) {
+    public double deposited(double amount, double balance) {
         if (amount < 0) {
             System.out.println("\nSummai jābūt pozitīvai");
         } else {
-            balance = amount;
+           balance = amount;
         }
-    }
+    };
 
-    public double withdrawal(double amount) {
+    public double withdrawal(double amount, double balance) {
         if (amount > 0) {
             System.out.println("\nSummai jābūt pozitīvai");
         } else if (amount < balance) {
             System.out.println("\nNepietiekami līdzekļi");
         } else {
-            balance = amount;
+           balance = amount;
         }
     }
 
-    public double getBalance() {
+    public double getBalance(double balance) {
         System.out.println(balance);
     }
 }
